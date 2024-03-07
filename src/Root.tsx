9995fec061280527214c209b3f960026b37aed46
Seongeun Yu <yse1027@gmail.com>
2024-03-07 18:14:34 +0900

    header, navigation 작업

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,65 @@
 import {Outlet} from 'react-router-dom';
 import Header from './components/Header';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+    open?: boolean;
+}>(({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
 
 function Root() {
+    const [open, setOpen] = React.useState(true);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <div>
-            <Header />
-            <main className='container'>
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <Header 
+                drawerWidth={drawerWidth}
+                open={open}
+                handleDrawerClose={handleDrawerClose} 
+                handleDrawerOpen={handleDrawerOpen} 
+            />
+            <Main open={open}>
+                <DrawerHeader />
                 <Outlet/>
-            </main>
-        </div>
+            </Main>
+        </Box>
     );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,163 @@
-import {Link} from 'react-router-dom';
+import {useNavigate, useLocation} from 'react-router-dom';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import PaymentIcon from '@mui/icons-material/Payment';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HomeIcon from '@mui/icons-material/Home';
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+    drawerWidth: number;
+}
 
-function Header() {
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open, drawerWidth }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
+interface IHeader {
+    drawerWidth: number;
+    open: boolean;
+    handleDrawerOpen: () => void;
+    handleDrawerClose: () => void;
+}
+
+function Header({drawerWidth, open, handleDrawerClose, handleDrawerOpen}:IHeader) {
+    const theme = useTheme();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { pathname } = location;
+
+    const handleListItemClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        uri: string,
+    ) => {
+        navigate(uri);
+    };
+
     return (
-        <header className='container'>
-            <Link to={`income`}><h2>수입</h2></Link>
-            <Link to={`expenditure`}><h2>지출</h2></Link>
-        </header>
+        <>
+            <AppBar position="fixed" open={open} drawerWidth={drawerWidth}>
+                <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap component="div">
+                    알로하 가계부
+                </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
+            >
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton
+                            selected={pathname === "/"}
+                            onClick={(event) => handleListItemClick(event, "")}
+                        >
+                            <ListItemIcon>
+                                <HomeIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'홈'} />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton
+                            selected={pathname === "/income"}
+                            onClick={(event) => handleListItemClick(event, "income")}
+                        >
+                            <ListItemIcon>
+                                <AttachMoneyIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'수입'} />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton
+                            selected={pathname === "/expenditure"}
+                            onClick={(event) => handleListItemClick(event, "expenditure")}
+                        >
+                            <ListItemIcon>
+                                <PaymentIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'지출'} />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+                <Divider />
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton
+                            selected={pathname === "/setting"}
+                            onClick={(event) => handleListItemClick(event, "setting")}
+                        >
+                            <ListItemIcon>
+                                <SettingsIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={'설정'} />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </>
     );
 }
 
