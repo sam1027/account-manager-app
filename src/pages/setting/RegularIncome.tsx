54c3205589b2335131dbb398b9db686a34e8896e
Seongeun Yu <yse1027@gmail.com>
2024-03-12 09:53:05 +0900

    정기수입 dialog 작업중

--- src/pages/setting/RegularIncome.tsx
+++ src/pages/setting/RegularIncome.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { GridColDef, GridRowsProp, GridValueFormatterParams } from "@mui/x-data-grid";
-import { randomCreatedDate, randomId } from "@mui/x-data-grid-generator";
+import { randomId } from "@mui/x-data-grid-generator";
 import Grid, { EAddType, EGridType } from "../../components/Grid";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { InputAdornment, MenuItem } from '@mui/material';
+import { MenuItem } from '@mui/material';
+import { MoneyFormatCustom } from '../../utils/Mui';
 
 interface IDialogItem {
     cycle: number;
@@ -23,9 +23,9 @@ interface IDialogItem {
 
 function RegularIncome() {
     const initialRows: GridRowsProp = [
-        { id: randomId(), cycle: '1', month: 12, date: 23, amount: 20000, source: '1', finance: '1', content: '기타1' },
-        { id: randomId(), cycle: '2', month: null, date: 23, amount: 1000, source: '2', finance: '2', content: '기타2' },
-        { id: randomId(), cycle: '3', month: null, date: null, amount: 50000, source: '3', finance: '3', content: '기타3' },
+        { id: randomId(), cycle: '1', month: 12, date: 23, amount: 20000, incomeSource: '1', finance: '1', content: '기타1' },
+        { id: randomId(), cycle: '2', month: null, date: 23, amount: 1000, incomeSource: '2', finance: '2', content: '기타2' },
+        { id: randomId(), cycle: '3', month: null, date: null, amount: 50000, incomeSource: '3', finance: '3', content: '기타3' },
     ];
 
     const _cycleCode = [
@@ -33,9 +33,20 @@ function RegularIncome() {
         { value: '2', label: '매달' },
         { value: '3', label: '매일' },
     ];
-    // const _monthCode = Array(9).fill(undefined).map(m => ({ value: m, label: m }));
     const _monthCode = Array.from({ length: 12 }, (_, i) => ({ value: `${i+1}`, label: `${i+1}` }));
     const _dateCode = Array.from({ length: 31 }, (_, i) => ({ value: `${i+1}`, label: `${i+1}` }));
+    const _incomeSourceCode = [
+        { value: '1', label: '월급' },
+        { value: '2', label: '불로소득' },
+        { value: '3', label: '부업' },
+        { value: '4', label: '알바비' },
+    ];
+    const _financeCode = [
+        { value: '1', label: '국민은행' },
+        { value: '2', label: '우리은행' },
+        { value: '3', label: '현금' },
+        { value: '4', label: '알바비' },
+    ];
     
     const columns: GridColDef[] = [
         { 
@@ -86,17 +97,12 @@ function RegularIncome() {
         },
         // 카테고리 항목으로 관리
         { 
-            field: 'source'
+            field: 'incomeSource'
             , headerName: '소득원'
             , width: 150
             , editable: false
             , type: 'singleSelect'
-            , valueOptions: [
-                { value: '1', label: '월급' },
-                { value: '2', label: '불로소득' },
-                { value: '3', label: '부업' },
-                { value: '4', label: '알바비' },
-            ]
+            , valueOptions: _incomeSourceCode
             , align: 'left'
         },
         // 카테고리 항목으로 관리
@@ -106,12 +112,7 @@ function RegularIncome() {
             , width: 150
             , editable: false
             , type: 'singleSelect'
-            , valueOptions: [
-                { value: '1', label: '국민은행' },
-                { value: '2', label: '우리은행' },
-                { value: '3', label: '현금' },
-                { value: '4', label: '알바비' },
-            ]
+            , valueOptions: _financeCode
             , align: 'left'
         },
         { 
@@ -124,6 +125,7 @@ function RegularIncome() {
     ];
 
     const [dialogOpen, setDialogOpen] = React.useState(false);
+    const [selectedCycle, setSelectedCycle] = React.useState("1");
 
     const handleDialogOpen = () => {
         setDialogOpen(true);
@@ -137,11 +139,13 @@ function RegularIncome() {
         handleDialogOpen();
     }
 
+    const handleCycleChangeEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedCycle(event.target.value);
+    }
+
 
     return (
         <div>
-            <h1>RegularIncome</h1>
-
             <Grid 
                 initialRows={initialRows} 
                 columns={columns} 
@@ -161,8 +165,8 @@ function RegularIncome() {
                         event.preventDefault();
                         const formData = new FormData(event.currentTarget);
                         const formJson = Object.fromEntries((formData as any).entries()) as IDialogItem;
+                        console.log(formJson);
                         const amount = formJson.amount;
-                        console.log(amount);
                         handleDialogClose();
                     },
                 }}
@@ -175,12 +179,14 @@ function RegularIncome() {
                     </DialogContentText> */}
 
                     <TextField
-                        id="outlined-select-currency"
+                        id="cycle"
                         select
                         label="실행 주기"
-                        defaultValue="1"
+                        defaultValue={selectedCycle}
                         margin="dense"
-                        helperText="Please select your currency"
+                        fullWidth
+                        style = {{width: 150}}
+                        onChange={handleCycleChangeEvent}
                     >
                         {_cycleCode.map((option) => (
                             <MenuItem key={option.value} value={option.value}>
@@ -189,12 +195,13 @@ function RegularIncome() {
                         ))}
                     </TextField>
                     <TextField
-                        id="outlined-select-currency"
+                        id="month"
+                        disabled={selectedCycle !== '1'}
                         select
-                        label="월"
-                        defaultValue="1"
+                        label="월(Month)"
+                        defaultValue={new Date().getMonth() + 1}
                         margin="dense"
-                        helperText="Please select your currency"
+                        style = {{width: 150, marginLeft: 10}}
                     >
                         {_monthCode.map((option) => (
                             <MenuItem key={option.value} value={option.value}>
@@ -203,12 +210,13 @@ function RegularIncome() {
                         ))}
                     </TextField>
                     <TextField
-                        id="outlined-select-currency"
+                        id="date"
+                        disabled={selectedCycle === '3'}
                         select
-                        label="일"
-                        defaultValue="1"
+                        label="일(Date)"
+                        defaultValue={new Date().getDate()}
                         margin="dense"
-                        helperText="Please select your currency"
+                        style = {{width: 150, marginLeft: 10}}
                     >
                         {_dateCode.map((option) => (
                             <MenuItem key={option.value} value={option.value}>
@@ -220,20 +228,58 @@ function RegularIncome() {
                         error={false}
                         autoFocus
                         required
-                        margin="dense"
+                        margin="normal"
                         id="amount"
                         name="amount"
                         label="금액"
                         fullWidth
                         variant="standard"
                         InputProps={{
-                            startAdornment: <InputAdornment position="end">$</InputAdornment>
+                            inputComponent: MoneyFormatCustom as any,
                         }}
+                        helperText="숫자를 입력해주세요."
+                    />
+                    <TextField
+                        id="incomeSource"
+                        select
+                        label="소득원"
+                        defaultValue={"1"}
+                        margin="normal"
+                        style = {{width: 200}}
+                    >
+                        {_incomeSourceCode.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <TextField
+                        id="finance"
+                        select
+                        label="금융기관"
+                        defaultValue={"1"}
+                        margin="normal"
+                        style = {{width: 200, marginLeft: 10}}
+                    >
+                        {_financeCode.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <TextField
+                        id="content"
+                        label="내용"
+                        multiline
+                        maxRows={4}
+                        fullWidth
+                        margin="normal"
+                        inputProps={{ maxLength: 300}}
                     />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleDialogClose}>Cancel</Button>
-                    <Button type="submit">Subscribe</Button>
+                    <Button type="submit">Save</Button>
                 </DialogActions>
             </Dialog>
         </div>
